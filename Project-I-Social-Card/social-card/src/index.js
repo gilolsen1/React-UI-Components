e5695a065c10516a85aaaaca100b57f093522633
Gilbert Olsen <gilbertolsen@Gilberts-MBP.hsd1.tx.comcast.net>
2019-05-08 21:51:52 -0400

    dont overreact

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+//import HeaderContainer from "./HeaderContainer.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
