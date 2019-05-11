c871f6af7a739284efad59ef4d073841c8feb2e4
Gilbert Olsen <gilbertolsen@Gilberts-MacBook-Pro.local>
2019-05-11 14:38:21 -0400

    created footer element

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import HeaderContainer from "./HeaderContainer.js";
-import ImageThumbnail from "./ImageThumbnail.js";
-import HeaderTitle from "./HeaderTitle.js";
-import HeaderContent from "./HeaderContent.js";
+import ImageThumbnail from "./ImageThumbnail.js"; //  lambda logo . js done:
+import HeaderContainer from "./HeaderContainer.js"; // red div
+import HeaderTitle from "./HeaderTitle.js"; //Lambda and date
+import HeaderContent from "./HeaderContent.js"; //Let's learn react
+import CardContainer from "./CardContainer.js"; //purple div
+import CardBanner from "./CardBanner.js"; // react logo
+import CardContent from "./CardContent"; // Get started with react  AND react makes it painless
+import Footer from "./Footer"; //footer image
 
 ReactDOM.render(<App />, document.getElementById("root"));
